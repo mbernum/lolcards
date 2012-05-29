3a9f4b42945c936b1c2774a7e2a747c4728a04d1
Mark Bernum <m.bernum@gmail.com>
2012-05-29 18:41:29 -0500

    Added decks. Game will gather a starting resource deck.

--- card.py
+++ card.py
@@ -1,4 +1,23 @@
+MAX_DECK_SIZE = 50
+
 class Card(object):
-    name = None
-    card_type = None
-    cost = None
+
+    def __init__(self):
+        self.name = None
+        self.card_type = None
+        self.cost = None
+
+
+class Deck(object):
+
+    def __init__(self):
+        self.name = None
+        self.kind = None
+        self.cards_in_deck = []
+
+    def get_card(self, card):
+        self.cards_in_deck.append(card)
+
+
+class MainDeck(Deck):
+    start_size = MAX_DECK_SIZE
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from card import Card
+from card import Card, MainDeck
 
 START_HAND_SIZE = 7
 TEST_ROUNDS = 4
@@ -8,10 +8,20 @@ class Player(object):
     def __init__(self, name):
         self.name = name
         self.hand = []
-        self.deck = []
+        self.main_deck = None
+        self.resource_deck = None
+        self.used_deck = None
 
     def get_card(self, card):
         self.hand.append(card)
+
+    def get_deck(self, deck, deck_type):
+        if deck_type == 'main':
+            self.main_deck = deck
+        elif deck_type == 'resource':
+            self.resource_deck = deck
+        elif deck_type == 'used':
+            self.used_deck = deck
 
 
 def opening_phase():
@@ -39,11 +49,22 @@ def get_random_card():
     return card
 
 
+def start_deck():
+    deck = MainDeck()
+    for opening_card in xrange(deck.start_size):
+        card = get_random_card()
+        deck.get_card(card)
+    return deck
+
+
 def start_game(players):
     for player in players:
         for x in xrange(START_HAND_SIZE):
             start_card = get_random_card()
             player.get_card(start_card)
+
+            starting_deck = start_deck()
+            player.get_deck(starting_deck, 'main')
 
 
 def main():
@@ -58,7 +79,6 @@ def main():
         move_phase()
         attack_phase()
         end_phase()
-
 
 if __name__ == '__main__':
     main()
