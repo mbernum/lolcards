b8c0d5fcac96b036442d3617e261ec735809203e
Mark Bernum <m.bernum@gmail.com>
2012-05-29 17:47:33 -0500

    Players get opening hand now.

--- card.py
+++ card.py
@@ -2,4 +2,3 @@ class Card(object):
     name = None
     card_type = None
     cost = None
-
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from card import Card
 
+START_HAND_SIZE = 7
+TEST_ROUNDS = 4
+
 
 class Player(object):
     def __init__(self, name):
         self.name = name
         self.hand = []
         self.deck = []
+
+    def get_card(self, card):
+        self.hand.append(card)
 
 
 def opening_phase():
@@ -28,8 +34,16 @@ def end_phase():
     pass
 
 
+def get_random_card():
+    card = Card()
+    return card
+
+
 def start_game(players):
-    pass
+    for player in players:
+        for x in xrange(START_HAND_SIZE):
+            start_card = get_random_card()
+            player.get_card(start_card)
 
 
 def main():
@@ -38,12 +52,13 @@ def main():
     players = [Player1, Player2]
     start_game(players)
 
-    while(True):
+    for game_round in xrange(TEST_ROUNDS):
         opening_phase()
         deploy_phase()
         move_phase()
         attack_phase()
         end_phase()
+
 
 if __name__ == '__main__':
     main()
